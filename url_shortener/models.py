5b834b435a7428a0fb607f5d15bdfa858f93964d
Trina <92634994+trinayau@users.noreply.github.com>
2022-05-30 21:26:37 +0100

    ready for deployment

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -7,7 +7,7 @@ from .extensions import db
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(512))
-    short_url = db.Column(db.String(3), unique=True)
+    short_url = db.Column(db.String(5), unique=True)
     visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
@@ -17,7 +17,7 @@ class Link(db.Model):
 
     def generate_short_link(self):
         characters = string.digits + string.ascii_letters
-        short_url = ''.join(choices(characters, k=3))
+        short_url = ''.join(choices(characters, k=5))
 
         link = self.query.filter_by(short_url=short_url).first()
 
